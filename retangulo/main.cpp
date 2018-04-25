cb05da9b7a7bf17324cc2389bbe0ccf2d8b49b7b
Tayná Arruda <taynarruda1@hotmail.com>
2018-04-25 06:24:26 -0300

    Add files via upload

--- retangulo/main.cpp
+++ retangulo/main.cpp
@@ -6,23 +6,27 @@ using namespace std;
 
 int main()
 {
+    Retangulo teste(2,3,5,5);
+    teste.impress();
+    cout<<endl;
+    cout<<"Area Ret Teste: "<<teste.calcArea()<<endl;
+    cout<<endl;
+    Retangulo r(0,0,3,4);
+    r.impress();
 
-   Retangulo r(0,0,3,4);
-   r.impress();
-
-   cout<<"Area Retangulo: "<<r.calcArea()<<endl;
-   cout<<endl;
-   r.translate(-3,4);
-   cout<<"Retangulo Transladado: "<<endl;
-   r.impress();
-   cout<<endl;
-   cout<<"Area Retangulo Transladado: "<<r.calcArea()<<endl;
-   cout<<endl;
-   Point CM;
-   CM.setXY((r.getLarg())/2,(r.getAlt())/2);
-   r.rotate(-30,CM);
-   cout<<"Retangulo rotacionado: "<<endl;
-   r.impress();
-   cout<<endl;
-   cout<<"Area ROt: "<<r.calcArea()<<endl;
+    cout<<"Area Retangulo: "<<r.calcArea()<<endl;
+    cout<<endl;
+    r.translate(-3,4);
+    cout<<"Retangulo Transladado: "<<endl;
+    r.impress();
+    cout<<endl;
+    cout<<"Area Retangulo Transladado: "<<r.calcArea()<<endl;
+    cout<<endl;
+    Point CM;
+    CM.setXY((r.getLarg())/2,(r.getAlt())/2);
+    r.rotate(30,CM);
+    cout<<"Retangulo rotacionado: "<<endl;
+    r.impress();
+    cout<<endl;
+    cout<<"Area rotacionada: "<<r.calcArea()<<endl;
 }
